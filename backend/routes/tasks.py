df2ded3f70264c67884c36f00c26ba7a6f1216d1
jondrielle wilson <jondriellewilson@Roberts-MacBook-Pro.local>
2026-06-13 20:11:27 -0500

    Adding post and get logic

--- backend/routes/tasks.py
+++ backend/routes/tasks.py
@@ -1,16 +1,32 @@
 from fastapi import APIRouter 
+from schemas.task import CreateTask, Task
 
 task_router = APIRouter()
 
-@task_router.get("/")
+tasks = []
+next_task_id = 1
+
+@task_router.get("/", response_model=list[Task])
 async def get_tasks():
-	return {"Message": "Create TODO List"}
+	return tasks
 
-@task_router.post("/add")
-async def add_task():
-	return {"Message" : "Add a task"}
+@task_router.post("/tasks", response_model=Task)
+async def add_task(task: CreateTask):
+	global next_task_id
 
-@task_router.delete("/delete")
+	new_task = Task(
+		id = next_task_id,
+		name = task.name,
+		description = task.description,
+		is_complete = False
+	)
+
+	tasks.append(new_task)
+
+	next_task_id += 1
+	return new_task
+
+@task_router.delete("/tasks/{task_id}")
 async def delete_task():
 	return {"Message" : "Delete a task"}
 
