b120f5e0a545af985815a902fa08e02ab6d02655
jondrielle wilson <jondriellewilson@Roberts-MacBook-Pro.local>
2026-06-15 20:58:10 -0500

    Finished patch http request logic

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -6,6 +6,7 @@ const tasks = ref([])
 const name = ref('')
 const description = ref('')
 const id = ref(0)
+const complete = ref('False')
 
 async function getTasks(){
   console.log("Getting Tasks from backend")
@@ -19,6 +20,8 @@ async function getTasks(){
     const result = await response.json();
 
     console.log(result);
+
+    tasks.value = result
   }
   catch(error){
     console.error(error.message);
@@ -26,6 +29,8 @@ async function getTasks(){
 }
 
 async function addTask(){
+  if (!name.value.trim()) return
+
   try{
     const response = await fetch("http://127.0.0.1:8000/tasks",{
       method:"POST",
@@ -74,13 +79,40 @@ async function deleteTask(id){
   }
 }
 
+async function updateTask(id, isComplete){
+  try{
+    const response = await fetch(`http://127.0.0.1:8000/tasks/${id}`,{
+      method:"PATCH",
+      headers:{
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        is_complete: isComplete
+      })
+    })
+    complete.value = isComplete
+    const updatedTask = await response.json()
+
+    const index = tasks.value.findIndex(task => task.id === id)
+
+    if (index !== -1){
+      tasks.value[index] = updatedTask
+    }
+
+    console.log(updatedTask)
+  }
+  catch(error){
+    console.error(error.message)
+  }  
+}
+
 onMounted(()=>{
   getTasks()
 })
 </script>
 
 <template>
-  <h1>TODO List!</h1>
+  <h1>TODO List</h1>
 
   <input v-model="name" placeholder="Task name"/>
   <input v-model="description" placeholder="Task Description"/>
@@ -91,7 +123,11 @@ onMounted(()=>{
   <div v-for="task in tasks" :key="task.id">
     <h4>{{task.name}}</h4>
     <p>{{task.description}}</p>
+
+    <div>Completed?</div>
+    <button @click="updateTask(task.id,!task.is_complete)">Is complete: {{task.is_complete}}</button>
     <button @click="deleteTask(task.id)">Delete Task</button>
+
   </div>
 </template>
 
